b51c9a97143eceabd0c5298ddebd8630fb6dfc0d
Ian Wakely <raveious.irw@gmail.com>
2017-01-31 17:16:09 -0500

    Adding in 10 min timer using a ros timer to issue a shutdown signal.

--- catkin_ws/src/perception/scripts/d3_nav.py
+++ catkin_ws/src/perception/scripts/d3_nav.py
@@ -5,8 +5,14 @@ import random
 from perception.srv import *
 from move_base_msgs.msg	import MoveBaseAction
 
+def _times_up(event):
+    rospy.loginfo("Shutdown timer issued")
+    rospy.signal_shutdown('Time is up!!')
+
 def D3Points_nav():
     rospy.init_node('d3_nav')
+
+    timer = rospy.Timer(rospy.Duration(600), _times_up, oneshot=True)
 
     DestSrv = rospy.ServiceProxy('d3_points', D3Points)
     mvbs = actionlib.SimpleActionClient('move_base', MoveBaseAction)
