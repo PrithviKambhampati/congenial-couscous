6d4cb829308932f31036b289260bb4bd7ffdbaf8
Prithvi Kambhampati <pkambham@mtu.edu>
2017-01-30 23:55:46 -0500

    laser scan program

--- catkin_ws/src/perception/scripts/random_map.py
+++ catkin_ws/src/perception/scripts/random_map.py
@@ -1,78 +1,41 @@
 #!/usr/bin/env python
 import rospy
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-import actionlib
-from actionlib_msgs.msg import *
-from geometry_msgs.msg import Pose, Point, Quaternion
+import random
+import sys
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+import time
 
-class GoToPose():
-    def __init__(self):
+def scan_callback(msg):
+    global g_range_ahead
+    g_range_ahead = min(msg.ranges)
 
-        self.goal_sent = False
+def getTime(data):
+    global last_time
+    last_time = rospy.get_time()
 
-	# What to do if shut down (e.g. Ctrl-C or failure)
-	rospy.on_shutdown(self.shutdown)
-	
-	# Tell the action client that we want to spin a thread by default
-	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-	rospy.loginfo("Wait for the action server to come up")
-
-	# Allow up to 5 seconds for the action server to come up
-	self.move_base.wait_for_server(rospy.Duration(5))
-
-    def goto(self, pos, quat):
-
-        # Send a goal
-        self.goal_sent = True
-	goal = MoveBaseGoal()
-	goal.target_pose.header.frame_id = 'map'
-	goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
-                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
-
-	# Start moving
-        self.move_base.send_goal(goal)
-
-	# Allow TurtleBot up to 60 seconds to complete task
-	success = self.move_base.wait_for_result(rospy.Duration(60)) 
-
-        state = self.move_base.get_state()
-        result = False
-
-        if success and state == GoalStatus.SUCCEEDED:
-            # We made it!
-            result = True
-        else:
-            self.move_base.cancel_goal()
-
-        self.goal_sent = False
-        return result
-
-    def shutdown(self):
-        if self.goal_sent:
-            self.move_base.cancel_goal()
-        rospy.loginfo("Stop")
-        rospy.sleep(1)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    last_time=0.0
+    g_range_ahead = 1.0
     try:
-        rospy.init_node('random_map', anonymous=False)
-        navigator = GoToPose()
-
-        # Customize the following values so they are appropriate for your location
-        position = {'x' : 2.5, 'y' : 2.5}
-        quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
-
-        rospy.loginfo("Go to (%s, %s) pose", position['x'], position['y'])
-        success = navigator.goto(position, quaternion)
-
-        if success:
-            rospy.loginfo("Hooray, reached the desired pose")
-        else:
-            rospy.loginfo("The base failed to reach the desired pose")
-
-        # Sleep to give the last log messages time to be sent
-        rospy.sleep(1)
+    	rospy.init_node("random_map")
+    	rospy.Subscriber("/front/scan", LaserScan, scan_callback)
+    	rate = rospy.Rate(50)
+	pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    	while not rospy.is_shutdown():
+    	    if g_range_ahead < 0.8:
+		print g_range_ahead
+    	        twist = Twist()
+    	        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+    	        twist.angular.z = 0.5
+		pub.publish(twist)
+	    elif g_range_ahead >= 0.8:
+		print g_range_ahead
+		twist = Twist()
+		twist.linear.x = 0.8
+                twist.angular.z = 0 
+		pub.publish(twist)
+    	    rate.sleep()
 
     except rospy.ROSInterruptException:
-	rospy.loginfo("Ctrl-C caught. Quitting")
+	pass
