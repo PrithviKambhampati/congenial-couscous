5e78fd2c3e1c180df4b01bd523264a10d0040983
Ian Wakely <raveious.irw@gmail.com>
2017-01-31 19:28:11 -0500

    Adding special launchfile and using more points.

--- catkin_ws/src/perception/scripts/d3_nav.py
+++ catkin_ws/src/perception/scripts/d3_nav.py
@@ -6,7 +6,7 @@ from perception.srv import *
 from move_base_msgs.msg	import MoveBaseAction
 
 def _times_up(event):
-    rospy.loginfo("Shutdown timer issued")
+    rospy.logwarn("Shutdown timer issued")
     rospy.signal_shutdown('Time is up!!')
 
 def D3Points_nav():
@@ -21,7 +21,7 @@ def D3Points_nav():
     rospy.loginfo("D3 Nav ready.")
 
     while not rospy.is_shutdown():
-        dest = random.choice(DestSrv(8, 1.5).destinations)
+        dest = random.choice(DestSrv(12, 2.5).destinations)
         rospy.loginfo(dest)
 
         mvbs.send_goal(dest)
